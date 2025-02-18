fd4a578b040a6c0543ce187549cb5aedd188bb06
aoneban <torgregion1@gmail.com>
2025-02-18 22:13:19 +0100

    feat: add some new task solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -99,8 +99,9 @@ function compareObjects(obj1, obj2) {
  *    isEmptyObject({}) => true
  *    isEmptyObject({a: 1}) => false
  */
-function isEmptyObject(/* obj */) {
-  throw new Error('Not implemented');
+function isEmptyObject(obj) {
+  const arr = Object.keys(obj);
+  return !arr.length;
 }
 
 /**
@@ -119,8 +120,8 @@ function isEmptyObject(/* obj */) {
  *    immutableObj.newProp = 'new';
  *    console.log(immutableObj) => {a: 1, b: 2}
  */
-function makeImmutable(/* obj */) {
-  throw new Error('Not implemented');
+function makeImmutable(obj) {
+  return Object.freeze(obj);
 }
 
 /**
